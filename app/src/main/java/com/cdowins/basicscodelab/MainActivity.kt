bb2f7fdbe7f8160bddbb240cb7f50806ad9e4d1c
Carlos9010 <dowins90@gmail.com>
2022-06-17 23:21:27 -0400

    Agregando acción al botón (Continue).

--- app/src/main/java/com/cdowins/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/cdowins/basicscodelab/MainActivity.kt
@@ -25,14 +25,59 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(names:List<String> = listOf("World", "Compose")) {
-     Column(modifier = Modifier.padding(vertical = 4.dp)) {
+fun MyApp() {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = {shouldShowOnboarding=false})
+    } else {
+        Greetings()
+    }
+}
+
+//------------------------------------------------------------------------------
+//Inicia primera pantalla.
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    // TODO: This state should be hoisted
+    //var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true, name = "Primera pantalla" )
+@Composable
+fun OnboardingPreview() {
+    BasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked ={})
+    }
+}
+//Termina primera pantalla
+//----------------------------------------------------------------------
+@Composable
+fun Greetings(names:List<String> = listOf("World", "Compose")){
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
         for (name in names) {
             Greeting(name = name)
         }
     }
 }
-
+//---------------------------------------------------------------------
+//Inicio de segunda pantalla
 @Composable
 fun Greeting(name: String) {
     val expanded= remember { mutableStateOf(false)}
@@ -56,40 +101,12 @@ fun Greeting(name: String) {
         }
     }
 }
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true, showSystemUi = true, name = "Segunda pantalla" )
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
-        MyApp()
+        Greetings()
     }
 }
-//------------------------------------------------------------------------------
-@Composable
-fun OnboardingScreen() {
-    // TODO: This state should be hoisted
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
-
-    Surface {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Welcome to the Basics Codelab!")
-            Button(
-                modifier = Modifier.padding(vertical = 24.dp),
-                onClick = { shouldShowOnboarding = false }
-            ) {
-                Text("Continue")
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun OnboardingPreview() {
-    BasicsCodelabTheme {
-        OnboardingScreen()
-    }
-}
+//Fin de segunda pantalla
+//-----------------------------------------------------------------------------
